93093077f037fef87860137bbf795b95acd82c40
murdoch2439 <murdochkhallz@gmail.com>
2023-09-12 18:35:47 +0100

    Adding new stuff

--- src/pages/privacy policy/PrivacyPolicy.tsx
+++ src/pages/privacy policy/PrivacyPolicy.tsx
@@ -9,7 +9,6 @@ export const PrivacyPolicy : FunctionComponent = () =>{
         <div className={"privacyContainer"}>
             <div className={"container__padding contact__content"}>
                 <h1 className={"second__title"} style={{color:"#2a374e", padding:"0.8rem 0"}}>Privacy policy</h1>
-
                 <p className={"paro"}>This page is being constructed...</p>
                 <p className={"paro"}>Come back later to check its availability.</p>
 
