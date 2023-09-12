6a3af32046824325260e60483483940c37682ee3
murdoch2439 <murdochkhallz@gmail.com>
2023-09-12 18:33:42 +0100

    First commit

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,11 +1,23 @@
 import React, {FunctionComponent} from "react"
 import "./contact.css"
+import {useNavigate} from "react-router-dom";
 
 const Contact : FunctionComponent = () =>{
+
+    const navigate= useNavigate()
+
     return(
         <div className={"contact__container"}>
             <div className={"container__padding contact__content"}>
 
+                <h1 className={"second__title"} style={{color:"#2a374e", padding:"0.8rem 0"}}>Contact</h1>
+                <p className={"paro"}>This page is being constructed...</p>
+                <p className={"paro"}>Come back later to check its availability.</p>
+                <div className={"call-to-action"}>
+                    {/*<div>*/}
+                    <button onClick={()=>navigate("/")} className={"call-to-action-button"}>Go home</button>
+                    {/*</div>*/}
+                </div>
             </div>
         </div>
     )
--- src/pages/privacy policy/PrivacyPolicy.tsx
+++ src/pages/privacy policy/PrivacyPolicy.tsx
@@ -8,9 +8,9 @@ export const PrivacyPolicy : FunctionComponent = () =>{
     return(
         <div className={"privacyContainer"}>
             <div className={"container__padding contact__content"}>
-                <h1 style={{color:"#2a374e"}}>Privacy policy</h1>
+                <h1 className={"second__title"} style={{color:"#2a374e", padding:"0.8rem 0"}}>Privacy policy</h1>
 
-                <p className={"paro"}>This page is being constructing...</p>
+                <p className={"paro"}>This page is being constructed...</p>
                 <p className={"paro"}>Come back later to check its availability.</p>
 
                 <div className={"call-to-action"}>
