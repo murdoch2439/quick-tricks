b596edf436bc7a379f88fc4921d7c0bb20ba14e6
murdoch2439 <murdochkhallz@gmail.com>
2023-09-12 14:16:10 +0100

    Adding faq page

---
+++ src/pages/faq/Faqage.tsx
@@ -0,0 +1,18 @@
+import React, {FunctionComponent} from "react"
+import "./faq.css"
+
+
+
+export const FaqPage: FunctionComponent =()=>{
+    return(
+        <div className={"faqContainer"}>
+            <div>
+                <h1>FAQ</h1>
+
+                <p>This page is being constructing...</p>
+                <p>Come back later to check its availability.</p>
+            </div>
+
+        </div>
+    )
+}
